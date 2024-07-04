bc8c50cfca160a069e10d8c0e2466dc67cf2f382
farha <farha.ahmed1@ucalgary.ca>
2024-07-03 20:36:08 -0600

    hello world

--- src/com/farha/calculator/Application.java
+++ src/com/farha/calculator/Application.java
@@ -2,7 +2,6 @@ package com.farha.calculator;
 
 public class Application {
     public static void main(String[] args) {
-        int age = 21;
-        System.out.println(age);
+        System.out.println("Hello, world!");
     }
 }
